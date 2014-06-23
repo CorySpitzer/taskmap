9e275d681828ae002079d6f751140e6bca6c1d9a
CorySpitzer <coryspitzer@gmail.com>
2014-06-23 18:50:56 -0400

    Write functionality for relationship adder methods.

--- tm_lib.rb
+++ tm_lib.rb
@@ -12,24 +12,25 @@ class Node
         @friends = friends
     end
     
+    # Relationship adders:
     def add_friend(new_friend)
-        # The current node and new_friend become friends:
-        # Adds new_friend to @friends array
-        # And adds self to new_friend.friends
-        # Removes other relationships with the current node
+        # The current node 
+        # and new_friend become friends
+        @friends           << new_friend
+        new_friend.friends << self
     end
-    
     def add_parent(new_parent)
-        # Gives the current node a new parent and new_parent a child:
-        # Adds new_parent to @parents array
-        # And adds self to new_parent.children
-        # Removes other relationships with the current node
+        # Gives the current node a new parent 
+        # and new_parent a child
+        @parents            << new_parent
+        new_parent.children << self
+    end
+    def add_child(new_child)
+        # Gives the current node a new child 
+        # and new_child a parent
+        @children         << new_child
+        new_child.parents << self
     end
     
-    def add_child(new_child)
-        # Gives the current node a new child and new_child a parent:
-        # Adds new_child to @children array
-        # And adds self to new_child.parents
-        # Removes other relationships with the current node
-    end
+    # Relationship removers:
 end 
