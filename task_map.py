de98ae8c017cb339096c6b5a46d3d43dd1defe4b
CorySpitzer <coryspitzer@gmail.com>
2014-09-07 17:02:43 -0400

    Write and debugged basic interactive prompt.

--- task_map.py
+++ task_map.py
@@ -1,5 +1,16 @@
 # task_map.py
 
+    # User XP   
+    # Begin at dashboard options to 
+    # view map and investigate nodes
+    # 
+
+
+# API
+    # Add node
+    # connect (child or parent)node
+    # disconnect
+
 from tm_lib import Node
 
 def main():
@@ -7,6 +18,43 @@ def main():
     
     print test_node.kids
     print test_node.parents
+    
+    current_node = test_node
+    help_string = ("(q)uit, (h)elp, (c)urrent node, (l)ist near nodes, " 
+                  "enter an integer to select a node, (m)ap the route "
+                  " between to points.\n")
+                  
+    # The input loop:
+    go = True
+    while go:
+        # Get commands: 
+        command = raw_input('Enter a command: ')
+        if command.lower()[0] == 'q':
+            go = False
+        elif command.lower()[0] == 'h':
+            print help_string
+        elif command.lower()[0] == 'c':
+            print current_node
+        elif command.lower()[0] == 'l':
+            print 'A node list...'
+            # print a list of nodes going to and coming from current
+            # with numbers for each to select
+        elif command.lower()[0] == 'm':
+            print 'The whole map...'
+            # prints, by level the nodes between the two given
+        else:
+            try:
+                # See if the entered command makes sense as an integer
+                node_number = int(command[0])
+            except ValueError:
+                print "Try a different command:"
+                print help_string
+            
+            
+
+## Content, leads_to
+#t1 = ['content', [t2, t3]]
+#t2 = ['c2', [t3, t]]
     
     
 main()
